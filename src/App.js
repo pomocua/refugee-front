d40b1c48e03641bd054913b69456941fafea88d2
Eugene Parhimovich <eugene.parhimovich@succraft.com>
2022-03-22 11:57:18 +0300

    updated main layout

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [applications, setApplications] = useState([])
-    // проверить актуальность всех полей
+  // проверить актуальность всех полей
   const [queryParams, setQueryParams] = useState({
     'page': 1,
     'take': 20,
     'name': searchValue,
-    'hasChildren' : false,
+    'hasChildren': false,
     'accommodation': false,
     'transportation': false,
     'location city': true,
@@ -93,73 +93,86 @@ const App = () => {
         <CircularProgress color="inherit"/>
       </Backdrop>
 
-      <Header/>
+      <div className="applications__wrapper">
+        <div className="applications__wrapper-top">
+          <Header/>
 
-      <section className="applications__sub-header">
-        <div className="applications__title-box wrapper">
-          <h1 className="applications__title">Applications list</h1>
-          <form className="applications__form" onSubmit={searchData}>
-            <input
-              className="applications__form-search"
-              type="text"
-              placeholder="Search all applications by name"
-              value={searchValue}
-              onChange={(event) => setSearchValue(event.target.value)}/>
-            <button className="applications__form-button" type="submit">
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="25" viewBox="0 0 152 448">
-                <path
-                  d="M148.75 240c0 2-1 4.25-2.5 5.75l-116.5 116.5c-1.5 1.5-3.75 2.5-5.75 2.5s-4.25-1-5.75-2.5l-12.5-12.5c-1.5-1.5-2.5-3.5-2.5-5.75 0-2 1-4.25 2.5-5.75l98.25-98.25-98.25-98.25c-1.5-1.5-2.5-3.75-2.5-5.75s1-4.25 2.5-5.75l12.5-12.5c1.5-1.5 3.75-2.5 5.75-2.5s4.25 1 5.75 2.5l116.5 116.5c1.5 1.5 2.5 3.75 2.5 5.75z"></path>
-              </svg>
-            </button>
-          </form>
+          <section className="applications__sub-header">
+            <div className="applications__title-box wrapper">
+              <h1 className="applications__title">Applications list</h1>
+              <form className="applications__form" onSubmit={searchData}>
+                <input
+                  className="applications__form-search"
+                  type="text"
+                  placeholder="Search all applications by name"
+                  value={searchValue}
+                  onChange={(event) => setSearchValue(event.target.value)}/>
+                <button className="applications__form-button" type="submit">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="25" viewBox="0 0 152 448">
+                    <path
+                      d="M148.75 240c0 2-1 4.25-2.5 5.75l-116.5 116.5c-1.5 1.5-3.75 2.5-5.75 2.5s-4.25-1-5.75-2.5l-12.5-12.5c-1.5-1.5-2.5-3.5-2.5-5.75 0-2 1-4.25 2.5-5.75l98.25-98.25-98.25-98.25c-1.5-1.5-2.5-3.75-2.5-5.75s1-4.25 2.5-5.75l12.5-12.5c1.5-1.5 3.75-2.5 5.75-2.5s4.25 1 5.75 2.5l116.5 116.5c1.5 1.5 2.5 3.75 2.5 5.75z"></path>
+                  </svg>
+                </button>
+              </form>
+            </div>
+          </section>
+
+          <section className="applications-filter">
+            <div className="wrapper">
+              <ul className="applications-filter__list">
+                <li className="applications-filter__item">
+                  <button
+                    className={queryParams.hasChildren
+                      ? 'applications-filter__button applications-filter__button_active'
+                      : 'applications-filter__button'}
+                    onClick={() => changeFilters('hasChildren')}
+                  >With kids
+                  </button>
+                </li>
+                <li className="applications-filter__item">
+                  <button
+                    className={queryParams.transportation
+                      ? 'applications-filter__button applications-filter__button_active'
+                      : 'applications-filter__button'}
+                    onClick={() => changeFilters('transportation')}
+                  >Transport only
+                  </button>
+                </li>
+                <li className="applications-filter__item">
+                  <button
+                    className={queryParams.accommodation
+                      ? 'applications-filter__button applications-filter__button_active'
+                      : 'applications-filter__button'}
+                    onClick={() => changeFilters('accommodation')}
+                  >Accommodation only
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="applications-list">
+            <div className="wrapper">
+              <CustomPaginationActionTable applicationList={applications} isLoading={isLoading}/>
+
+            </div>
+          </section>
         </div>
-      </section>
-
-      <section className="applications-filter">
-        <div className="wrapper">
-          <ul className="applications-filter__list">
-            <li className="applications-filter__item">
-              <button
-                className={queryParams.hasChildren
-                  ? 'applications-filter__button applications-filter__button_active'
-                  : 'applications-filter__button'}
-                onClick={() => changeFilters('hasChildren')}
-              >With kids</button>
-            </li>
-            <li className="applications-filter__item">
-              <button
-                className={queryParams.transportation
-                  ? 'applications-filter__button applications-filter__button_active'
-                  : 'applications-filter__button'}
-                onClick={() => changeFilters('transportation')}
-              >Transport only</button>
-            </li>
-            <li className="applications-filter__item">
-              <button
-                className={queryParams.accommodation
-                  ? 'applications-filter__button applications-filter__button_active'
-                  : 'applications-filter__button'}
-                onClick={() => changeFilters('accommodation')}
-              >Accommodation only</button>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <section className="applications-list">
-        <div className="wrapper">
-          <CustomPaginationActionTable applicationList={applications} isLoading={isLoading}/>
+        <div className="applications__wrapper-bottom">
           <Pagination className="pagination"
-                      onChange={(event, value) => changePage(event, value)}
-                      // изменить count на количество страниц из ответа
-                      count={Math.floor('count' / 20)}
-                      defaultPage={1}
-                      siblingCount={1}
-                      boundaryCount={1}
-                      color="primary"
+            onChange={(event, value) => changePage(event, value)}
+                      // вместо count поставить данные о количестве постов
+            count={Math.floor('count' / 20)}
+            defaultPage={1}
+            siblingCount={1}
+            boundaryCount={1}
+            color="primary"
           />
         </div>
-      </section>
+      </div>
+
+
+
 
     </div>
   )
